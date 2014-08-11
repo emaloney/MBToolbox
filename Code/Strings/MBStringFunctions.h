5e89ae90f6e60fe443597d3f52f46a4cd340424b
Evan Maloney <emaloney@gilt.com>
2014-08-11 11:15:08 -0400

    the documentation was making promises the implementation didn't keep
    rectify this

--- Code/Strings/MBStringFunctions.h
+++ Code/Strings/MBStringFunctions.h
@@ -12,10 +12,27 @@
 /******************************************************************************/
 
 //!< given any object, outputs a string; nil or NSNull yields empty strings
-static inline NSString* MBForceString(id x) { return (x ? (x == [NSNull null] ? @"" : [x description]) : @""); }
+static inline NSString* MBForceString(id x)
+{
+    NSString* retVal = @"";
+    if (x && x != [NSNull null]) {
+        NSString* desc = [x description];
+        if (desc) {
+            retVal = desc;
+        }
+    }
+    return retVal;
+}
 
 //!< given any object, forces it to a string and trims whitespace from the ends
-static inline NSString* MBTrimString(id x) { return (x ? (x == [NSNull null] ? @"" : [[x description] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]) : @""); }
+static inline NSString* MBTrimString(id x)
+{
+    NSString* retVal = MBForceString(x);
+    if (retVal.length > 0) {
+        retVal = [retVal stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    }
+    return retVal;
+}
 
 /******************************************************************************/
 #pragma mark Stringification

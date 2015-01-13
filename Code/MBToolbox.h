be9c691de1e3db0bba2c1329a3b8b5070d96bcfb
Evan Maloney <emaloney@gilt.com>
2015-01-13 17:35:02 -0500

    remove hacky way of building libraries
    add support for building frameworks
    
    use either cocoapods or frameworks for integrating

--- Code/MBToolbox.h
+++ Code/MBToolbox.h
@@ -12,6 +12,14 @@
 
 #else
 
+#import <Foundation/Foundation.h>
+
+//! Project version number for MBToolbox.
+FOUNDATION_EXPORT double MBToolboxVersionNumber;
+
+//! Project version string for MBToolbox.
+FOUNDATION_EXPORT const unsigned char MBToolboxVersionString[];
+
 //
 // NOTE: This header file is indended for external use. It should *not* be
 //       included from within code in the Mockingbird Toolbox itself.

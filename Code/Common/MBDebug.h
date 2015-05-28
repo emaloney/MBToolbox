da9314f5cd1b23e5036a15b5de251fdef0cc9212
Evan Maloney <emaloney@gilt.com>
2015-05-28 17:55:57 -0400

    remove profiling code; add MB prefix for macros

--- Code/Common/MBDebug.h
+++ Code/Common/MBDebug.h
@@ -28,57 +28,10 @@
 #pragma mark Triggering the debugger
 /******************************************************************************/
 
-#define __triggerDebugBreakMsg(f, m)    if (DEBUG_MASTER) { consoleLog(@"Breakpoint hit!\n\nEntering debugger due to %@%@\n\n\t• Type 'up 2' to see breakpoint position\n\t• Type 'fin' and then 'fin' again to select calling stack frame\n\t• Type 'c' to continue execution\n\n", f, (m ? [NSString stringWithFormat:@" -- %@", m] : @"")); kill(getpid(), SIGSTOP); }
+#define __MBTriggerDebugBreakMsg(f, m)    if (DEBUG_MASTER) { NSLog(@"Breakpoint hit!\n\nEntering debugger due to %@%@\n\n\t• Type 'up 2' to see breakpoint position\n\t• Type 'fin' and then 'fin' again to select calling stack frame\n\t• Type 'c' to continue execution\n\n", f, (m ? [NSString stringWithFormat:@" -- %@", m] : @"")); kill(getpid(), SIGSTOP); }
 
-#define triggerDebugBreak()             __triggerDebugBreakMsg(@"triggerDebugBreak()", nil)
+#define MBTriggerDebugBreak()             __triggerDebugBreakMsg(@"triggerDebugBreak()", nil)
 
-#define triggerDebugBreakIf(x)          if (DEBUG_MASTER && x) { NSString* call = [NSString stringWithFormat:@"triggerDebugBreakIf(%s)", #x]; __triggerDebugBreakMsg(call, nil); }
+#define MBTriggerDebugBreakIf(x)          if (DEBUG_MASTER && x) { NSString* call = [NSString stringWithFormat:@"triggerDebugBreakIf(%s)", #x]; __triggerDebugBreakMsg(call, nil); }
 
-#define triggerDebugBreakMsg(m)         __triggerDebugBreakMsg(@"triggerDebugBreakMsg()", m)
-
-/******************************************************************************/
-#pragma mark Code profiling
-/******************************************************************************/
-
-/*
- When in debug mode (via DEBUG_MASTER) provides a simple mechanism to profile
- a chunk of code. Start by doing:
-
-     MBProfilingTimerStart(@"critical path 1");
-
-         ...code you want to profile...
-
-     MBProfilingTimerEnd();
-
- When the code is run, check the console for output.
-
- Note that you can only call MBProfilingTimerStart() once within a given
- scope. If you want to profile a different section of code within the same
- code scope, you must use MBProfilingTimerRestart(), eg.:
-
-     MBProfilingTimerRestart(@"critical path 2");
-
-         ...other code you want to profile...
-
-     MBProfilingTimerEnd();
- */
-#if (DEBUG_MASTER)
-#define MBProfilingTimerStart(x) \
-NSTimeInterval __MBProfilingTimerEnd = 0; \
-NSTimeInterval __MBProfilingTimerStart = [NSDate timeIntervalSinceReferenceDate]; \
-NSString* __MBProfilingTimerName = x; \
-consoleLog(@"Profiling timer:\t%@ starting", __MBProfilingTimerName);
-
-#define MBProfilingTimerEnd() \
-__MBProfilingTimerEnd = [NSDate timeIntervalSinceReferenceDate]; \
-consoleLog(@"Profiling timer:\t%@\t%g\tseconds elapsed", __MBProfilingTimerName, (__MBProfilingTimerEnd - __MBProfilingTimerStart));
-
-#define MBProfilingTimerRestart(x) \
-__MBProfilingTimerEnd = 0; \
-__MBProfilingTimerStart = [NSDate timeIntervalSinceReferenceDate]; \
-__MBProfilingTimerName = x;
-#else
-#define MBProfilingTimerStart(x)
-#define MBProfilingTimerEnd()
-#define MBProfilingTimerRestart(x)
-#endif
+#define MBTriggerDebugBreakMsg(m)         __triggerDebugBreakMsg(@"triggerDebugBreakMsg()", m)
